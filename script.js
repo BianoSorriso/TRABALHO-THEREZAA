8a7d41eb13ff7a5f45cf1343d6f17209b791a6b4
Fabiano Bastos <fabiano__bastos@hotmail.com>
2024-09-05 00:13:39 -0300

    Atualização Carrinho de Compras
    Atualização do Carrinho de compras com inclusão das fotos do produto e soma dos valores no carrinho.

--- script.js
+++ script.js
@@ -1,8 +1,93 @@
-function addToCart(productName, productPrice) {
-    alert('$productName} adicionado ao carrinho por ${productPrice}')
+let cartItems = [];
+let totalAmount = 0;
+
+
+
+
+function addToCart(productName, productPrice, productImage) {
+    let item = cartItems.find(item => item.name === productName);
+
+    if (item) {
+        item.quantity += 1;
+        totalAmount += productPrice;
+    } else {
+        cartItems.push({ name: productName, price: productPrice, image: productImage, quantity: 1 });
+        totalAmount += productPrice;
+    }
+
+    updateCartList();
+    document.getElementById('cart-item-count').textContent = `(${cartItems.length})`;
+    alert(`${productName} adicionado ao carrinho por R$${formatCurrency(productPrice)}`);
 }
 
 
+function updateCartList() {
+    const cartItemsList = document.getElementById('cart-items-list');
+    const cartDropdown = document.getElementById('cart-dropdown');
+    cartItemsList.innerHTML = '';
+
+    if (cartItems.length === 0) {
+        
+        cartDropdown.style.display = 'none';
+    } else {
+        
+        cartItems.forEach(item => {
+            const listItem = document.createElement('li');
+            listItem.classList.add('cart-item');
+
+            const img = document.createElement('img');
+            img.src = item.image;
+            img.alt = item.name;
+            img.classList.add('cart-item-image');
+
+            const text = document.createElement('span');
+            text.textContent = `${item.name} - R$${formatCurrency(item.price)} x${item.quantity}`;
+
+            listItem.appendChild(img);
+            listItem.appendChild(text);
+            cartItemsList.appendChild(listItem);
+        });
+
+        const totalItem = document.createElement('li');
+        totalItem.textContent = `Total: R$${formatCurrency(totalAmount)}`;
+        totalItem.classList.add('total');
+        cartItemsList.appendChild(totalItem);
+
+        
+    }
+}
+
+
+function formatCurrency(value) {
+    return value.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
+
+
+document.getElementById('cart').addEventListener('mouseover', () => {
+    if (cartItems.length > 0) {
+        document.getElementById('cart').classList.add('show');
+    }
+});
+document.getElementById('cart').addEventListener('mouseleave', () => {
+    document.getElementById('cart').classList.remove('show');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//COISAS RELACIONADAS AO BANNER
 
 const img_banner = document.querySelector('#imagem-banner');
 const container_banner = document.querySelector('.banner');
